8d2f22a787ca5468588914415c43299b18387c98
Max Boettcher <mvboettcher@gmail.com>
2020-11-19 18:10:06 -0500

    converts SideBarItem to functional component

--- src/SidebarItem/index.jsx
+++ src/SidebarItem/index.jsx
@@ -1,5 +1,5 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
 import ListItem from '@material-ui/core/ListItem';
@@ -7,46 +7,40 @@ import ListItemText from '@material-ui/core/ListItemText';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { removeHTMLTags } from '../helpers';
 
-class SidebarItem extends Component {
-  constructor() {
-    super()
-    this.state = {}
-  }
+const SidebarItem = (props) => {
+  const { classes, _note, _index, selectedNoteIndex } = props
 
-  selectNote = (n, i) => this.props.selectNote(n, i)
-  deleteNote = (note) => {
+  const selectNote = (n, i) => props.selectNote(n, i)
+
+  const deleteNote = (note) => {
     if (window.confirm(`Are you sure you want to delete: ${note.title}?`)) {
-      this.props.deleteNote(note)
+      props.deleteNote(note)
     }
   }
 
-  render() {
-    const { classes, _note, _index, selectedNoteIndex } = this.props
-
-    return (
-      <div key={_index}>
-        <ListItem
-          className={classes.listItem}
-          selected={selectedNoteIndex === _index}
-          alignItems="flex-start"
+  return (
+    <div key={_index}>
+      <ListItem
+        className={classes.listItem}
+        selected={selectedNoteIndex === _index}
+        alignItems="flex-start"
+      >
+        <div
+          className={classes.textSection}
+          onClick={() => selectNote(_note, _index)}
         >
-          <div
-            className={classes.textSection}
-            onClick={() => this.selectNote(_note, _index)}
-          >
-            <ListItemText
-              primary={_note.title}
-              secondary={removeHTMLTags(_note.body.substring(0, 30)) + '...'}
-            />
-          </div>
-          <DeleteIcon
-            className={classes.deleteIcon}
-            onClick={() => this.deleteNote(_note)}
+          <ListItemText
+            primary={_note.title}
+            secondary={removeHTMLTags(_note.body.substring(0, 30)) + '...'}
           />
-        </ListItem>
-      </div>
-    )
-  }
+        </div>
+        <DeleteIcon
+          className={classes.deleteIcon}
+          onClick={() => deleteNote(_note)}
+        />
+      </ListItem>
+    </div>
+  )
 }
 
 export default withStyles(styles)(SidebarItem)
