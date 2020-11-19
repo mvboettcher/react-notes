75cfe39ff96ce90b4497a9fd45e5e3bce45812c4
Max Boettcher <mvboettcher@gmail.com>
2020-11-18 21:23:01 -0500

    connects firestore to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import firebase from 'firebase'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      selectNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    }
+  }
+
+  componentDidMount() {
+    firebase
+      .firestore()
+      .collection('notes')
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data()
+          data['id'] = _doc.id
+          return data
+        })
+        console.log(notes)
+        this.setState({ notes: notes })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        React App
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyA4jA_FCXtVJYTswmEQ82fwP8_UEMJ7xFM",
+  authDomain: "react-notes-27bc6.firebaseapp.com",
+  databaseURL: "https://react-notes-27bc6.firebaseio.com",
+  projectId: "react-notes-27bc6",
+  storageBucket: "react-notes-27bc6.appspot.com",
+  messagingSenderId: "802216494874",
+  appId: "1:802216494874:web:843088f3aa7411ca40cea3",
+  measurementId: "G-KXFT5LHVS9"
+});
+firebase.analytics();
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +26,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
